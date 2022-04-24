c7dde280fe6fa0e3eb6d6e965f4ac9124f3b8d7c
mardyc754 <mar754dyc@gmail.com>
2022-04-24 20:05:06 +0200

    Initial commit, test of database connections

---
+++ Project/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConnectingToSQLServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+
+            //your connection string 
+            string connectionString = @"Data Source=X240\SQLEXPRESS;Initial Catalog = AdventureWorks2016;Integrated Security = True;";
+            //create instanace of database connection
+            SqlConnection conn = new SqlConnection(connectionString);
+
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.WriteLine("Naciśnij q, aby zakończyć działanie programu...");
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
